d5c3e63553c06cdbbd6317b5a8b80033384a2925
Rachel Yengle <137534126+RachelYengle@users.noreply.github.com>
2024-08-18 13:54:23 -0700

    Update Data Case 1.1.sql

--- Data Case 1.1.sql
+++ Data Case 1.1.sql
@@ -1,17 +1,18 @@
-CREATE TABLE TEMP.c1_Prep_Top_10_Vendors_by_Billing_Amount as
-SELECT VendorNumber, 
-VendorName,
+--A critcal vendor is a vendor with >$50,000 purchase amount--
+CREATE TABLE TEMP.c1_Prep_Billing_Amount as
+SELECT VendorNumber,VendorName,
  SUM(Dollars) AS 'TotalBilling'
  FROM VendorInvoicesDec
  WHERE Approval = 'Frank Delahunt'
  GROUP BY VendorNumber, VendorName
- ORDER BY TotalBilling DESC LIMIT 10;
+ HAVING SUM(Quantity) > 50000
+ ORDER BY TotalBilling DESC;
 
-CREATE TABLE TEMP.c1_Prep_Top_10_Vendors_by_Purchase_Orders_Amount as
-SELECT VendorNumber, 
-VendorName,
+CREATE TABLE TEMP.c1_Prep_Purchase_Orders_Amount as
+SELECT VendorNumber, VendorName,
  COUNT(DISTINCT PONumber) AS 'TotalPurchaseOrders'
  FROM VendorInvoicesDec
  WHERE Approval = 'Frank Delahunt'
  GROUP BY VendorNumber, VendorName
- ORDER BY TotalPurchaseOrders DESC LIMIT 10;
+ HAVING SUM(Quantity) > 50000
+ ORDER BY TotalPurchaseOrders DESC;
