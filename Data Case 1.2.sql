a7cadd28bf25e79731b76dfb37d0edef9cccb25e
Rachel Yengle <137534126+RachelYengle@users.noreply.github.com>
2024-08-18 13:44:57 -0700

    Update Data Case 1.2.sql

--- Data Case 1.2.sql
+++ Data Case 1.2.sql
@@ -1,17 +1,8 @@
---A critcal vendor is a vendor with >$50,000 of inventory purchased from.--
-CREATE TABLE TEMP.c1_Prep_Purchase_Amount AS
-SELECT 
-    VendorNumber, 
-    VendorName,
-    SUM(Quantity) AS 'TotalPurchaseAmount'
-FROM 
-    VendorInvoicesDec
-WHERE 
-    Approval = 'Frank Delahunt'
-GROUP BY 
-    VendorNumber, 
-    VendorName
-HAVING 
-    SUM(Quantity) > 50000
-ORDER BY 
-    TotalPurchaseAmount DESC;
+CREATE TABLE TEMP.c1_Prep_Top_10_Vendors_by_Purchase_Amount as
+SELECT VendorNumber, 
+VendorName,
+ SUM(Quantity) AS 'TotalPurchaseAmount'
+ FROM VendorInvoicesDec
+ WHERE Approval = 'Frank Delahunt'
+ GROUP BY VendorNumber, VendorName
+ ORDER BY TotalPurchaseAmount DESC LIMIT 10;
